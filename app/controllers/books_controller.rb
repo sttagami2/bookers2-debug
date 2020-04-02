8df9d04e73c01abb213cb3e2ff7f3d1193a85204
vagrant <vagrant@localhost.localdomain>
2020-04-02 03:21:05 +0000

    [Update] 次こそdeviseとUserを紐付ける

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,12 @@ class BooksController < ApplicationController
   end
 
   def edit
-  	@book = Book.find(params[:id])
+		@book = Book.find(params[:id])
+		if @book.user == current_user
+			render :edit
+		else
+			redirect_to books_path
+		end
   end
 
 
